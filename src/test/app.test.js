8a1881ff10f03634669fdbf49bb169ac28a4e056
Mangiulia <146093289+Mangiulia@users.noreply.github.com>
2023-11-13 12:06:03 +0200

    Update app.test.js

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,20 +1,53 @@
-const assert = require('assert')
-const compress = require('../app.js')
+function rle(input, compress = true) {
+  if (typeof input !== 'string' && !(input instanceof String)) {
+    throw new Error('InvalidType');
+  }
 
-describe('test compression', function(){
-	it('returns empty string', function(){
-		assert.strictEqual(compress('', true), '')
-	})
-	it('returns a4b5c2d4', function(){
-		assert.strictEqual(compress('aaaabbbbbccdddd', true), 'a4b5c2d4')
-	})
-	it('throws an exception', function(){
-		assert.throws(() => compress([], true), { message : 'InvalidType' })
-	})
-	it('returns aaabbbbccc', function(){
-		assert.strictEqual(compress('a3b4c3', false), 'aaabbbbccc')
-	})
-	it('returns abcdd', function(){
-		assert.strictEqual(compress('abcdd'), 'a1b1c1d2')
-	})
-})
+  if (compress) {
+    return compressRLE(input);
+  } else {
+    return decompressRLE(input);
+  }
+}
+
+function compressRLE(input) {
+  if (input.length === 0) {
+    return '';
+  }
+
+  let result = '';
+  let count = 1;
+
+  for (let i = 1; i < input.length; i++) {
+    if (input[i] === input[i - 1]) {
+      count++;
+    } else {
+      result += input[i - 1] + (count > 1 ? count : '');
+      count = 1;
+    }
+  }
+
+  result += input[input.length - 1] + (count > 1 ? count : '');
+
+  return result;
+}
+
+function decompressRLE(input) {
+  if (input.length === 0) {
+    return '';
+  }
+
+  let result = '';
+  let count = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if (isNaN(parseInt(input[i]))) {
+      count = parseInt(input[i]) || 1;
+      result += input[i].repeat(count);
+    }
+  }
+
+  return result;
+}
+
+module.exports = rle;
